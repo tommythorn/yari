8363fb7b7da93abac173f04a108df58608a05d2c
Tommy Thorn - Silvia <tommy-git@thorn.ws>
2008-01-01 17:03:18 -0800

    Added a memory tester

--- testcases/memorytester.c
+++ testcases/memorytester.c
@@ -1,111 +1,69 @@
 /*
- * This is intended to be an extensive RAM tester ... mostly really just
- * testing the SRAM controller.
+ * Test memory
+ *
+ * Construct a 512 KiB linked ring and chase through it
+ *
+ * This however, only tests word accesses.
  */
 
-/* First a support library. */
+#include <stdio.h>
 
-static inline
-putchar(unsigned ch)
+// 512 KiB = 128 KiW = 2^17
+
+//#define K 17
+#define K 16
+#define N (1 << (K))
+
+unsigned **ring;
+
+extern unsigned int _end;
+
+int main(int argc, char **argv)
 {
-  if (ch == '\n')
-    putchar('\r');
+    unsigned i, j, cur, *p;
+    unsigned k;
 
-#define SER_OUTBUSY() (*(volatile unsigned *)0xFF000000 != 0)
-#define SER_OUT(data) (*(volatile unsigned *)0xFF000000 = (data))
-  while (SER_OUTBUSY())
-    ;
-  SER_OUT(ch);
-}
+    printf("Memory Testing\n");
 
-static void put_unsigned(unsigned n)
-{
-  // (unsigned)-1 == 4294967295
-  static unsigned powers10[] = {
-    1000000000,
-    100000000,
-    10000000,
-    1000000,
-    100000,
-    10000,
-    1000,
-    100,
-    10,
-    1,
-    0
-  };
-  unsigned *p;
-  unsigned leading_zero = 1;
+    ring = (unsigned **) &_end;
 
-  if (!n) {
-    putchar('0');
-    return;
-  }
+    for (k = 10; k <= K; ++k) {
+        unsigned n = 1 << k;
 
-  for (p = powers10; *p; ++p) {
-    unsigned scale = *p;
-    unsigned i, t;
+        // XXX Ok, this is a totally broken way of doing this, but I
+        // don't want to implement DIV right now which is needed for
+        // the correct way.
 
-    for (i = 0; n >= scale; ++i, n -= scale)
-      ;
-    if (i != 0 || !leading_zero) {
-      putchar('0' + i);
-      leading_zero = 0;
-    }
-  }
-}
+        printf("Build linked ring of size %d\n", n);
+        cur = 0;
+        ring[cur] = (unsigned *) &ring[cur];
+        for (i = 1; i < n; ++i)
+            ring[i] = 0;
 
-void myputs(char *s)
-{
-  while (*s)
-    putchar(*s++);
-}
-
-#define RS232IN_DATA (*(unsigned *) 0xFF000004)
-#define RS232IN_TAG  (*(unsigned *) 0xFF000008)
-#define TSC          (*(unsigned *) 0xFF00000C)
-
-
-
-int
-main()
-{
-    unsigned  * const sram = (unsigned *) 0x40000000;
-    unsigned i, count = 0;
-
-    put_unsigned(123);
-    myputs("\nOk, are we ready to roll?\n");
-
-    while (SER_OUTBUSY()) ;
-    SER_OUT('O');
-    while (SER_OUTBUSY()) ;
-    SER_OUT('k');
-    while (SER_OUTBUSY()) ;
-    SER_OUT('!');
-    while (SER_OUTBUSY()) ;
-    SER_OUT('\r');
-    while (SER_OUTBUSY()) ;
-    SER_OUT('\n');
-
-
-    for (i = 1;; ++i) {
-        if (i == 0x1000) {
-            i = 1;
-            put_unsigned(++count);
+        for (i = 1; i < n - 128; ++i) {
+            unsigned next;
+            do {
+                next = random() & (n - 1);
+            } while (ring[next]);
+            ring[next] = (unsigned *) &ring[cur];
+            //printf("%d -> %d\n", next, cur);
+            cur = next;
+            if ((i &  127) == 0)
+                printf("%d ", n - i);
         }
-        sram[i] = i;
-        while (SER_OUTBUSY()) ;
-        if (sram[i-1] != i-1) {
-            while (SER_OUTBUSY()) ;
-            SER_OUT("0123456789abcdef"[(i >> 12) & 15]);
-            while (SER_OUTBUSY()) ;
-            SER_OUT("0123456789abcdef"[(i >>  8) & 15]);
-            while (SER_OUTBUSY()) ;
-            SER_OUT("0123456789abcdef"[(i >>  4) & 15]);
-            while (SER_OUTBUSY()) ;
-            SER_OUT("0123456789abcdef"[(i >>  0) & 15]);
-            while (SER_OUTBUSY()) ;
-            SER_OUT('\n');
+        ring[0] = (unsigned *) &ring[cur];
+        //printf("0 -> %d\n", cur);
+        printf("\n");
+
+        for (i = 0; i < 10; ++i) {
+            for (p = ring[0], j = n - 128; j; --j)
+                p = * (unsigned **) p;
+            printf("%d.. ", i);
         }
+
+        if (p == ring[0])
+            printf("Succeeded\n");
+        else
+            printf("Failure\n");
     }
 }
