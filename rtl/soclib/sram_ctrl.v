1ed316aaf3bd4ca15524d916ca8fde2969acac84
Tommy Thorn - Silvia <tommy-git@thorn.ws>
2008-01-08 19:27:37 -0800

    Brand new SRAM controller
      * Supports the new pipelined burst protocol
      * All outputs and inputs are registered
      * Supports one wait state
      * Support sustained writes every other cycle
      * Burst reads at maximum read bandwidth
    
      As noted in the RTL, this version wastes once cycle between burst
      reads. This is easily fixable, but not done in this change

--- rtl/soclib/sram_ctrl.v
+++ rtl/soclib/sram_ctrl.v
@@ -1,6 +1,6 @@
 /* -----------------------------------------------------------------------
  *
- *   Copyright 2004 Tommy Thorn - All Rights Reserved
+ *   Copyright 2007 Tommy Thorn - All Rights Reserved
  *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -9,298 +9,136 @@
  *   (at your option) any later version; incorporated herein by reference.
  *
  * -----------------------------------------------------------------------
- *
- *
- * SRAM controller
- *
- * The is tailored for the Nios Dev. Kit, Cyclone edition which is
- * equipped with two IDT71V416 (S10PH Z0030P), a 256k x 16 SRAM.  The
- * SRAMs share all signals except the data bus and the bute enables
- * (however the data bus and byte enables are shared with the flash
- * ram and the ethernet adapter).
- *
- * The protocol is very simple: to perform a certain action, setup the
- * appropriate signals and raise the corresponding strobe signal.
- * Hold all signals while wait is active.  If the request was a read,
- * the data will be available in the first cycle after the wait.  If a
- * write isn't in progress and no higher priority reads are attempted
- * in the same cycle, the read will never see a wait and the data will
- * be available in the following cycle.
- *
- * TODO:
- * - Make it a real one-hot state machine (explicit idle state)
- * - Support the Flash RAM and the Ethernet device which are on the
- *   same bus.
- *
- * Some technical stuff about the SRAM protocol
- * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- * Reading is very easy; just write the address and pick up the data after
- * 10 ns (assuming OE is active and WE is inactive).
- *   1: [Optionally OE_n = 0, WE_n = 1, data_dir = IN]
- *      fse_addr = rd_addr;
- *      WAIT 10 ns (max{tRC,tAA,tOE})
- *
- *   2: data_o = fse_data
- *
- * Write cycle is trickier because we have to toggle WE:
- *
- *   1: fse_addr = wr_addr, OE_n = 1
- *      WAIT 5 ns (tOHZ)
- *
- *   2: WE_n = 0, fse_data_dir = OUT, fse_data = wr_data
- *      WAIT 8 ns (tWP)
- *
- *   3: WE_n = 1, OE_n = 0
- *      WAIT 5 ns (tOE)
- *
- *   4: data_dir = IN
- *
- * For writes in a row we can do
- *
- *   1: fse_addr = wr_addr, OE_n = 1
- *      WAIT 5 ns (tOHZ)
- *
- *   2: WE_n = 0, fse_data_dir = OUT, fse_data = wr_data
- *      WAIT 8 ns (tWP)
- *
- *      WHILE wr_req & ~rd_req DO
- *        XXX I'm banking on tWR=0 working!
- *
- *     3: WE_n = 1, fse_addr = wr_addr, fse_data = wr_data
- *        WAIT ??
- *
- *     4: WE_n = 0
- *        WAIT 5 ns (tOE)
- *
- *   5: data_dir = IN
- *
- * I probably could drive data out in step 1 but it
- * would be fighting the the output driver
- *
- *
  */
 
 `timescale 1ns/10ps
-`include "pipeconnect.h"
-module sram_ctrl(
-        input  wire        clk,
+module sram_ctrl
+   (input  wire        clock
+   ,input  wire        rst
 
-        // One read/write port (Latency 1, if not busy writing)
-        input  wire `REQ   sram_req,
-        output wire `RES   sram_res,
+   ,output             mem_waitrequest
+   ,input        [1:0] mem_id
+   ,input       [29:0] mem_address
+   ,input              mem_read
+   ,input              mem_write
+   ,input       [31:0] mem_writedata
+   ,input        [3:0] mem_writedatamask
+   ,output reg  [31:0] mem_readdata
+   ,output reg   [1:0] mem_readdataid = 0
 
-        // Flash-SRAM-Ethernet bus
-        output wire [22:0] sram_a,
-        inout  wire [31:0] sram_d,
-        output wire        sram_cs_n,
-        output wire  [3:0] sram_be_n,
-        output wire        sram_oe_n,
-        output wire        sram_we_n);
+   ,output reg  [29:0] sram_a
+   ,inout  wire [31:0] sram_d
+   ,output reg         sram_cs_n = 1
+   ,output reg   [3:0] sram_be_n
+   ,output reg         sram_oe_n = 1
+   ,output reg         sram_we_n = 1
+   );
 
-   parameter debug = 0;
+   parameter burst_bits = 2;
+   parameter need_wait  = 0;
 
-   pipechecker pipechecker("sram_ctrl", clk, sram_req, sram_res);
+   parameter burst_length = 1 << burst_bits;
 
-   /*
-    * OUT OF DATE COMMENT
-    * Writing takes a state machine [, reading is immediate (pipelined).]
-    * The write state machine is one-hot encoded.
-    * XXX Make it truly one-hot and make a read state also.
-    * State 0: loads the write address, marks it busy writing
-    * State 1: asserts we, load data out
-    * State 2: deassert we
-    * State 3: turn the data bus back to input, acknowledge the write
-    */
+   parameter S_IDLE     = 0;
+   parameter S_READ     = 1;
+   parameter S_READWAIT = 2;
+   parameter S_WRITE1   = 3;
+   parameter S_WRITE2   = 4;
+   reg [3:0] state      = S_IDLE;
 
-   /********************************************************************************
-    *                           Port outputs
-    *******************************************************************************/
+   wire      sel        = mem_address[29:26] == 'h4;
+   reg [burst_bits:0] cnt = ~0;
+   reg        int_we_n     = 1;
+   reg [1:0]  pendingid;
+   reg [31:0] sram_dout;
 
-/*
+   assign    mem_waitrequest   = state != S_IDLE;
+   // XXX This coupling between oe# and the tri-state driver is perhaps a bit unsound
+   // clearly there will be fighting for a short period
+   assign    sram_d            = sram_oe_n ? sram_dout : 'hZ;
 
-WISHFUL THINKING:
+   always @(negedge clock)
+      sram_we_n <= int_we_n;
 
-Two sequential writes + One read
-                   Drive addr  Write data
-clock      ~~~\______/~~~~~~~\______/~~~~~~\______/~~~~~~\______/~~~~~~\______/~~~~~~\______/~~~~~~\______/~~~~~~\______
+   always @(posedge clock) begin
+      mem_readdataid <= 0;
+      case (state)
+      S_IDLE:
+         if (mem_read) begin
+            pendingid <= mem_id;
+            sram_a    <= mem_address;
+            sram_cs_n <= 0;
+            sram_oe_n <= 0;
+            sram_be_n <= 0;
+            int_we_n  <= 1;
+            cnt       <= burst_length - 1;
+            state     <= need_wait ? S_READWAIT : S_READ;
+         end else if (mem_write) begin
+            sram_a    <= mem_address;
+            sram_dout <= mem_writedata;
+            sram_be_n <= ~mem_writedatamask;
+            sram_cs_n <= 0;
+            sram_oe_n <= 1;
+            int_we_n  <= 0;
+            state     <= S_WRITE1;
+         end else begin
+            sram_cs_n <= 0;
+            sram_oe_n <= 1;
+            int_we_n  <= 1;
+         end
 
-write      ______________/~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\_______________________________________
-addr       <## Addr 0 ##><###### Addr1 #############><###### Addr 2 #############><## Addr 3 ##>------------------------
-wr_data    --------------<###### Data1 #############><###### Data 2 #############>--------------------------------------
-rd_data    ______________<## Data 0 ##>_________________________________________________________<## Data 3 ##>__________
-hold       ______________/~~~~~~~~~~~~\______________/~~~~~~~~~~~~\_____________________________________________________
+      S_READWAIT:
+         state <= S_READ;
 
-sram_addr  <## Addr 0 ##><###### Addr1 #############><###### Addr 2 #############><## Addr 3 ##>------------------------
-sram_data* ZZZZZZZZZZZZZZZZZZZZZZZZ><## Data1 ###><ZZZZZZZZZZZZ><## Data2 ###><ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ
-sram_we_n  ~~~~~~~~~~~~~~~~~~~~~~~~~\____________/~~~~~~~~~~~~~~\____________/~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-sram_be_n  _________________________<## BE1 #####>______________<## BE2 #####>__________________________________________
+      S_READ:
+         if (!cnt[burst_bits]) begin
+            cnt            <= cnt - 1;
+            sram_a         <= sram_a + 1;
+            mem_readdata   <= sram_d;
+            mem_readdataid <= pendingid;
+            state          <= need_wait ? S_READWAIT : S_READ;
+         end else
+            // XXX save a cycle and pick up new reads and writes from here. That however requires
+            // adjusting waitrequest al
+            state          <= S_IDLE;
 
-write_     _________________________/~~~~~~~~~~~~\
+      S_WRITE1: begin
+         if (need_wait)
+            state    <= S_WRITE2;
+         else begin
+            int_we_n <= 1;
+            state    <= S_IDLE;
+         end
+      end
 
- (*) sram_data as driven, not actual line.
-
-QUESTION: is it safe to switch address lines simultaneously with the we_n?  Probably not....
-
-TURNS OUT TO BE SAFE I HAVE TO MAKE READ TAKE TWO CYCLES AND WRITE THREE :-(
-
-
-Two sequential writes + One read
-                   Drive addr  Write data
-clock      ~~~\______/~~~~~~~\______/~~~~~~\______/~~~~~~\______/~~~~~~\______/~~~~~~\______/~~~~~~\______/~~~~~~\______
-
-write      ______________/~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\_______________________________________
-addr       <## Addr 0 ##><###### Addr1 #############><###### Addr 2 #############><## Addr 3 ##>------------------------
-wr_data    --------------<###### Data1 #############><###### Data 2 #############>--------------------------------------
-rd_data    ______________<## Data 0 ##>_________________________________________________________<## Data 3 ##>__________
-hold       ______________/~~~~~~~~~~~~~~~~~~~~~~~~~~\______________/~~~~~~~~~~~~\_____________________________________________________
-
-sram_addr  <## Addr 0 ##><###### Addr1 #############><###### Addr 2 #############><## Addr 3 ##>------------------------
-sram_data* ZZZZZZZZZZZZZZZZZZZZZZZZ><## Data1 ###><ZZZZZZZZZZZZ><## Data2 ###><ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ
-sram_we_n  ~~~~~~~~~~~~~~~~~~~~~~~~~\____________/~~~~~~~~~~~~~~\____________/~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-sram_be_n  _________________________<## BE1 #####>______________<## BE2 #####>__________________________________________
-sram_oe_n  ______________/~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\_______________________________________
-
-write      _________________________/~~~~~~~~~~~~\______________________
-write_     _______________________________________/~~~~~~~~~~~~\______________________
-*/
-
-// `define FAST_READ 1
-
-   reg         write = 0;
-   reg         write_= 0;
-   reg         read  = 0;
-   reg         read_ = 0;
-
-   /*
-    * The SRAM is currently always selected, which is necessary to get
-    * the low latency. Once the flash RAM and the ethernet port come
-    * into play, we can no longer afford such luxury.
-    */
-   wire [31:0] addr   = sram_req`A;
-   wire        reqR   = sram_req`R;
-   wire        reqW   = sram_req`W;
-   wire        hold   = sram_res`HOLD;
-
-   reg         reqR_  = 0;
-   reg  [31:0] sram_d_= 0;
-
-   assign      sram_res`RD   = reqR_ ? sram_d_ : 0;
-   assign      sram_res`HOLD = reqW & ~write_ | reqR & ~read;
-
-   assign      sram_cs_n =  0;
-   assign      sram_we_n = ~write;
-   assign      sram_oe_n =  sram_req`W;
-   assign      sram_be_n = (sram_req`W & ~write_) ? ~sram_req`WBE : 0;
-   assign      sram_d    =  sram_req`W            ?  sram_req`WD  : 32'hZZZZZZZZ;
-   assign      sram_a    =  sram_req`A;
-
-   always @(posedge clk) begin
-      if (debug)
-        $display("%5d SRAM_CTRL: req`R %d  W %d R %d Hold %d SRAM A %8x D %8x BE %x OE %x WE %x",
-                 $time, read, reqW, reqR, hold, addr, sram_d, sram_be_n, sram_oe_n, sram_we_n);
-
-      sram_d_ <= sram_d;
-      reqR_   <= reqR;
-      read    <= reqR;
-      read_   <= read;
-      write   <= reqW & ~(write|write_);
-      write_  <= write;
-
-      if (debug & read)
-        $display("%5d SRAM_CTRL: Read [%8x] -> %8x (sram_a %x)",
-                 $time, sram_req`A, sram_d, sram_a);
-
-      if (debug & write)
-        $display("%5d SRAM_CTRL: Write %8x & %x -> [%8x] (sram_a %x)",
-                 $time, sram_req`WD, sram_req`WBE, sram_req`A, sram_a);
-     end
-
-`ifdef SIMULATE_MAIN
-   pipechecker check("sram_ctrl", clk, sram_req, sram_res);
-   initial
-     if (debug)
-       $monitor("%5d SRAM_CTRL' W %d R %d Hold %d SRAM A %8x D %8x BE %x OE %x WE %x",
-                $time, reqW, reqR, hold, sram_a, sram_d, sram_be_n, sram_oe_n, sram_we_n);
-`endif
-endmodule
-
-`ifdef SIMULATE_SRAM_CTRL
-module test();
-   reg         clk = 1;
-   reg         rst = 0;
-
-
-   wire [22:0] sram_a;
-   wire [31:0] sram_d;
-   wire        sram_cs_n;
-   wire [ 3:0] sram_be_n;
-   wire        sram_oe_n;
-   wire        sram_we_n;
-
-   reg    `REQ sram_req;
-   wire   `RES sram_res;
-
-   idt71v416s10 u35(sram_d[15: 0], sram_a[19:2], sram_we_n, sram_oe_n, sram_cs_n,
-                    sram_be_n[0], sram_be_n[1]); // Yep, strange order...
-   idt71v416s10 u36(sram_d[31:16], sram_a[19:2], sram_we_n, sram_oe_n, sram_cs_n,
-                    sram_be_n[2], sram_be_n[3]);
-
-   sram_ctrl sram_ctrl(clk, rst,
-                       sram_req, sram_res,
-                       sram_a, sram_d, sram_cs_n, sram_be_n, sram_oe_n, sram_we_n);
-
-   // XXX For some reason they don't seem to display correctly in $monitor
-   // statements.  Icarus bug?
-   wire        sram_req_r    = sram_req`R;
-   wire        sram_req_w    = sram_req`W;
-   wire        sram_res_hold = sram_res`HOLD;
-   wire [31:0] sram_res_rd   = sram_res`RD;
-
- `define MAGIC_ADDR1 'h1730
- `define MAGIC_DATA1 'h87654321
- `define MAGIC_ADDR2 'h1729
- `define MAGIC_DATA2 'h98765432
-
-   always #20 clk = ~clk;
-   initial begin
-      $display("Time Ck r w H data");
-      $monitor("%4d %x  %x %x %x %x",
-               $time, clk,
-               sram_req_r, sram_req_w, sram_res_hold, sram_res_rd);
-
-      #0 rst = 1; sram_req = 0;
-      #120 rst = 0;
-
-      $display("%4d write %8x -> [%4x]", $time, `MAGIC_DATA1, `MAGIC_ADDR1);
-      sram_req`A   = `MAGIC_ADDR1 * 4;
-      sram_req`WD  = `MAGIC_DATA1;
-      sram_req`WBE = ~0;
-      sram_req`W   = 1;
-      #80
-      wait (~sram_res_hold);
-      sram_req`W = 0;
-      $display("%4d write %8x -> [%4x]", $time, `MAGIC_DATA2, `MAGIC_ADDR2);
-      sram_req`A   = `MAGIC_ADDR2 * 4;
-      sram_req`WD  = `MAGIC_DATA2;
-      sram_req`WBE = ~0;
-      sram_req`W   = 1;
-      #80
-      wait (~sram_res_hold);
-      sram_req`W = 0;
-      $display("%4d read [%4x]", $time, `MAGIC_ADDR2);
-      sram_req`A = `MAGIC_ADDR2 * 4;
-      sram_req`R = 1;
-      #40
-        if (sram_res_rd != `MAGIC_DATA2)
-          $display("FAILURE %8x ");
-        sram_req`R = 0;
-      $display("%4d read [1729]", $time);
-      sram_req`A = 'h1729 * 4;
-      sram_req`R = 1;
-      #40 sram_req`R = 0;
-      #320
-
-      #2400 $finish;
+      S_WRITE2: begin
+         int_we_n <= 1;
+         state    <= S_IDLE;
+      end
+      endcase
    end
-endmodule
+
+//`define DEBUG_SRAM 1
+`ifdef DEBUG_SRAM
+   always @(negedge clock)
+         $display("%05d  SRAM cs# %x a %x d %x we# %x oe# %x", $time,
+                  sram_cs_n, sram_a, sram_d, sram_we_n, sram_oe_n);
+
+   always @(posedge clock) begin
+      if (!mem_waitrequest & sel & mem_read)
+         $display("%05d  sram[%x] -> ? for %d", $time,
+                  {mem_address,2'd0}, mem_id);
+
+      if (!mem_waitrequest & sel & mem_write)
+         $display("%05d  sram[%x] <- %8x/%x", $time,
+                  {mem_address,2'd0}, mem_writedata, mem_writedatamask);
+
+      if (mem_readdataid)
+         $display("%05d  sram[%x] -> %8x for %d", $time,
+                  32'h3fff_fffc + (sram_a << 2), mem_readdata, mem_readdataid);
+
+      $display("%05d  SRAM cs# %x a %x d %x we# %x oe# %x", $time,
+               sram_cs_n, sram_a, sram_d, sram_we_n, sram_oe_n);
+   end
 `endif
+endmodule
