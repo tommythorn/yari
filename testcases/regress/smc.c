d765fb50ab9c207f203aa9e363da5e839951b310
Tommy Thorn <tommy-git@thorn.ws>
2008-04-19 00:19:00 -0700

    testcases/regress: smc.c test I$ behaviour

---
+++ testcases/regress/smc.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/*
+ * Test self-modifying code
+ */
+
+int return_const(void);
+asm("return_const:");
+asm(".set noreorder");
+asm("jr $31");
+asm("li $2, 1729");
+asm(".set reorder");
+
+
+int main()
+{
+    int i;
+
+    for (i = 0; i < 100; ++i) {
+        short *p = (short *) return_const;
+        p[3] = i;
+        if (return_const() != i) {
+            printf("Failure\n");
+            return 1;
+        }
+    }
+
+    printf("Success\n");
+
+    return 0;
+}
