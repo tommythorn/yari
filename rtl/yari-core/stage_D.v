fb520ee079baa422f85fc493eea9482b60dc54d1
Tommy Thorn <tommy-git@thorn.ws>
2008-07-03 22:58:57 -0700

    CORE: update copyright

--- rtl/yari-core/stage_D.v
+++ rtl/yari-core/stage_D.v
@@ -1,6 +1,6 @@
 // -----------------------------------------------------------------------
 //
-//   Copyright 2004,2007 Tommy Thorn - All Rights Reserved
+//   Copyright 2004,2007,2008 Tommy Thorn - All Rights Reserved
 //
 //   This program is free software; you can redistribute it and/or modify
 //   it under the terms of the GNU General Public License as published by
