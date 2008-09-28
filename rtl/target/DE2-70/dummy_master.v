9a5cc3fc21d1ba86d29bea67a7486adc6fa11c58
Tommy Thorn <tommy-git@thorn.ws>
2008-09-27 17:05:50 -0700

    TARGET DE2-70: Memory tester was broken -> Fixed and sped up
    Also made it more likely to read to check a better mix of read and
    write operations
    
    It still has the minor problem that it will detect errors until the
    writes have touched all locations

--- rtl/target/DE2-70/dummy_master.v
+++ rtl/target/DE2-70/dummy_master.v
@@ -14,12 +14,14 @@ module dummy_master(
            ,input          [31:0] mem_readdata
            ,input           [1:0] mem_readdataid
 
+
            ,output reg     [31:0] errors = 0
            );
 
-   reg [29:0] wp = 0, rp = 0, vp = 0;
+   reg [18:0] wp = 0, rp = 0, vp = 0;
    reg [31:0] data = 0;
    reg [32:0] lfsr = 0;
+   reg mismatch_pre = 0, mismatch = 0;
 
    always @(posedge clock) if (reset) begin
       wp <= 0;
@@ -29,13 +31,18 @@ module dummy_master(
       errors <= 0;
       mem_read <= 0;
       mem_write <= 0;
+      mismatch_pre <= 0;
+      mismatch <= 0;
    end else begin
       mem_id <= 1;
 
       lfsr <= {lfsr[31:0], ~lfsr[32] ^ lfsr[19]};
 
       if (mem_readdataid == 1) begin
-         if (mem_readdata != {vp,~vp[3:0]})
+         // Delay to help fMAX
+         mismatch_pre <= mem_readdata != {vp,~vp[3:0]};
+         mismatch <= mismatch_pre;
+         if (mismatch)
             errors <= errors + 1;
          vp <= vp + 1;
       end
@@ -44,7 +51,7 @@ module dummy_master(
          mem_read          <= 0;
          mem_write         <= 0;
 
-         if (lfsr[0] | lfsr[1] | lfsr[2] | lfsr[3] | ~lfsr[4]) begin
+         if (~lfsr[4]) begin
             mem_writedata     <= {wp, ~wp[3:0]};
 
             mem_writedatamask <= /*lfsr[4:1]*/ ~0;
