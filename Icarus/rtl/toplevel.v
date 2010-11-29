ebf90f966e1511586ab0b51780d39bb2b73ac05e
Tommy Thorn <tommy-git@thorn.ws>
2010-11-28 20:37:54 -0800

    Icarus: get Icarus simulation functional again
    Unfortunately it default to running the tinymon.c firmware which
    probably isn't all the useful in general.

---
+++ Icarus/rtl/toplevel.v
@@ -0,0 +1,297 @@
+// -----------------------------------------------------------------------
+//
+//   Copyright 2004,2007,2008 Tommy Thorn - All Rights Reserved
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation, Inc., 53 Temple Place Ste 330,
+//   Bostom MA 02111-1307, USA; either version 2 of the License, or
+//   (at your option) any later version; incorporated herein by reference.
+//
+// -----------------------------------------------------------------------
+//
+// Main module
+
+
+/*
+  4000_0000 - 400F_FFFF Extern SRAM (1 MiB)
+  BFC0_0000 - BFC0_3FFF Boot ROM (16 KiB) (Preloaded I$ cache)
+  FF00_0000 - FF00_1FFF Peripherals
+                        Read             Write
+                      0 rs232out busy    rs232out data
+                      1 rs232in  data
+                      2 rs232in  count
+                      3 TSC
+ */
+
+`timescale 1ns/10ps
+`include "pipeconnect.h"
+
+`ifdef SIMULATE_MAIN
+`define BLOCKRAM 1
+`endif
+
+module main(// Clock and reset
+             input  wire        clkin        // K5  PLL input clock (50 MHz)
+            ,output wire        pld_clkout   // L8  Clock to zero-skew buffer Lancelot board
+            ,input  wire        pld_clkfb    // L14 Feedback from pld_clkout to PLL2
+            ,input  wire        reset_n      // C4  CPU Reset button
+
+            // Lancelot VGA interface
+            ,output wire  [7:0] vga_r        // VGA red
+            ,output wire  [7:0] vga_g        // VGA green
+            ,output wire  [7:0] vga_b        // VGA blue
+            ,output wire        vga_hs       // VGA horz sync
+            ,output wire        vga_vs       // VGA vert sync
+            ,output wire        vga_blank_n  // VGA DAC force blank
+            ,output wire        vga_sync_n   // VGA sync enable
+            ,output wire        vga_sync_t   // VGA sync on R/G/B
+            ,output wire        vga_m1       // VGA color space config
+            ,output wire        vga_m2       // VGA color space config
+
+`ifdef PS2_ENABLED
+            // PS/2 keyboard/mouse
+            ,output             ps2_sel      // PS/2 port input/output select
+            ,inout              ps2_kclk     // PS/2 keyboard clock
+            ,inout              ps2_kdata    // PS/2 keyboard data
+            ,inout              ps2_mclk     // PS/2 mouse clock
+            ,inout              ps2_mdata    // PS/2 mouse data
+`endif
+
+            // Push buttons LEDs 7-segments
+            ,input  wire  [3:0] sw           // Pushbutton switches
+            ,output       [7:0] led          // Debugging LEDs
+            ,output wire  [7:0] s7_0         // Debugging 7-segment LEDs
+            ,output wire  [7:0] s7_1         // --
+
+`ifndef SIMULATE_MAIN
+            // Flash-SRAM-Ethernet bus
+            ,output wire [22:0] fse_a        // Mainboard common bus address
+            ,inout  wire [31:0] fse_d        // Mainboard common bus data
+            ,output wire        flash_cs_n   // Flash ROM CS#
+            ,output wire        enet_aen     // Ethernet Access Enable
+            ,output wire        sram_cs_n    // SRAM CS#
+            ,output wire  [3:0] sram_be_n    // SRAM byte enables
+            ,output wire        sram_oe_n    // SRAM OE#
+            ,output wire        sram_we_n    // SRAM WE#
+`endif
+
+`ifdef SERIAL_ENABLED
+            // Serial IO
+            ,output             ttya_dcd     // Console DCD#
+            ,output             ttya_txd     // Console TxD
+            ,input              ttya_rxd     // Console RxD
+            ,input              ttya_dtr     // Console DTR#
+            ,output             ttya_dsr     // Console DSR#
+            ,input              ttya_rts     // Console RTS#
+            ,output             ttya_cts     // Console CTS#
+            ,output             ttya_ri      // Console RI#
+`endif
+            ,output             ttyb_txd     // Debug TxD
+            ,input              ttyb_rxd     // Debug RxD
+
+`ifdef CF_ENABLED
+            // CompactFlash slot
+            ,output [10:0]      cf_a         // CompactFlash address bus
+            ,inout [15:0]       cf_d         // CompactFlash data bus
+            ,input              cf_rdy       // CompactFlash RDY
+            ,input              cf_wait_n    // CompactFlash WAIT#
+            ,output             cf_ce1_n     // CompactFlash CE1#
+            ,output             cf_ce2_n     // CompactFlash CE2#
+            ,output             cf_oe_n      // CompactFlash OE#
+            ,output             cf_we_n      // CompactFlash WE#
+            ,output             cf_reg_n     // CompactFlash REG#
+            ,input              cf_cd1_n     // CompactFlash card detect
+`endif
+            // SDRAM
+            // Audio
+            // Ethernet
+            // Flash memory
+            );
+   // ------------------------------------------------------------------------
+   //  Reset
+   // (there is option for extending the reset internally here, if necessary)
+   // ------------------------------------------------------------------------
+//    wire          rst = ~reset_n;
+
+   // ------------------------------------------------------------------------
+   //  PLLs and clock distribution
+   // ------------------------------------------------------------------------
+
+   // XXX Note 45 MHz appears to be the limit for the sram with just 1
+   // wait state (I happen to know the memory is good for much more
+   // with a trick I can't explain why works.)
+   parameter FREQ = 45_454_545; // match clock frequency
+   parameter BPS  =    115_200; // Serial speed
+
+   wire [ 7:0]   rs232out_d;
+   wire          rs232out_w;
+   wire          rs232out_busy;
+
+   wire [ 7:0]   rs232in_data;
+   wire          rs232in_attention;
+
+   wire          mem_waitrequest;
+   wire    [1:0] mem_id;
+   wire   [29:0] mem_address;
+   wire          mem_read;
+   wire          mem_write;
+   wire   [31:0] mem_writedata;
+   wire    [3:0] mem_writedatamask;
+   wire   [31:0] mem_readdata;
+   wire    [1:0] mem_readdataid;
+
+   wire `REQ     rs232_req;
+   wire `RES     rs232_res;
+
+`ifdef SIMULATE_MAIN
+   // Flash-SRAM-Ethernet bus
+   wire [22:0]   fse_a;        // Mainboard common bus address
+   wire [31:0]   fse_d;        // Mainboard common bus data
+   wire          flash_cs_n;   // Flash ROM CS#
+   wire          enet_aen;     // Ethernet Access Enable
+   wire          sram_cs_n;    // SRAM CS#
+   wire [3:0]    sram_be_n;    // SRAM byte enables
+   wire          sram_oe_n;    // SRAM OE#
+   wire          sram_we_n;    // SRAM WE#
+
+`ifndef BLOCKRAM
+   idt71v416s10 u35(fse_d[15: 0], fse_a[19:2], sram_we_n, sram_oe_n, sram_cs_n,
+                    sram_be_n[0], sram_be_n[1]); // Yep, strange order...
+   idt71v416s10 u36(fse_d[31:16], fse_a[19:2], sram_we_n, sram_oe_n, sram_cs_n,
+                    sram_be_n[2], sram_be_n[3]);
+`endif
+`endif
+
+   assign        flash_cs_n = 1;  // Disable flash ROM
+   assign        enet_aen   = 1;  // Disable Ethernet
+
+`ifdef SIMULATE_MAIN
+   reg           clk = 0;
+`else
+   wire          clk;
+   wire          pll_locked;
+
+   pll pll(
+        .inclk0(clkin),         // 50 MHz input clock
+        .c0(clk),
+        .locked(pll_locked)
+        );
+
+   assign  led = {rled[0],rled[1],rled[2],rled[3],rled[4],rled[5],rled[6],rled[7]};
+   reg  [7:0] rled = 0;
+`endif // SIMULATE_MAIN
+
+   reg  [ 3:0] reset_count_up = 0;
+`ifdef SIMULATE_MAIN
+   wire        rst = 0;
+`else
+   wire        rst = ~reset_n /*reset_count_up != 15*/;
+`endif
+
+   always @(posedge clk)
+      if (~reset_n)
+        reset_count_up <= 0;
+      else if (rst) begin
+         if (reset_count_up == 14)
+           $display("Reset count %d", reset_count_up);
+         reset_count_up <= reset_count_up + 1'd1;
+      end
+
+   yari yari_inst(
+         .clock(clk)
+        ,.rst(rst)
+
+        ,.mem_waitrequest(mem_waitrequest)
+        ,.mem_id(mem_id)
+        ,.mem_address(mem_address)
+        ,.mem_read(mem_read)
+        ,.mem_write(mem_write)
+        ,.mem_writedata(mem_writedata)
+        ,.mem_writedatamask(mem_writedatamask)
+        ,.mem_readdata(mem_readdata)
+        ,.mem_readdataid(mem_readdataid)
+
+        ,.peripherals_req(rs232_req)
+        ,.peripherals_res(rs232_res)
+        );
+
+`ifdef SIMULATE_MAIN
+   blockram blockram_inst
+      (.clock(clk)
+      ,.rst(rst)
+      ,.mem_waitrequest(mem_waitrequest)
+      ,.mem_id(mem_id)
+      ,.mem_address(mem_address)
+      ,.mem_read(mem_read)
+      ,.mem_write(mem_write)
+      ,.mem_writedata(mem_writedata)
+      ,.mem_writedatamask(mem_writedatamask)
+      ,.mem_readdata(mem_readdata)
+      ,.mem_readdataid(mem_readdataid)
+      );
+
+   defparam blockram_inst.INIT_FILE="`SRAM_INIT";
+`else
+   sram_ctrl sram_ctrl_inst
+      (.clock(clk)
+      ,.rst(rst)
+      ,.mem_waitrequest(mem_waitrequest)
+      ,.mem_id(mem_id)
+      ,.mem_address(mem_address)
+      ,.mem_read(mem_read)
+      ,.mem_write(mem_write)
+      ,.mem_writedata(mem_writedata)
+      ,.mem_writedatamask(mem_writedatamask)
+      ,.mem_readdata(mem_readdata)
+      ,.mem_readdataid(mem_readdataid)
+
+      ,.sram_a(fse_a[19:2])
+      ,.sram_d(fse_d)
+      ,.sram_cs_n(sram_cs_n)
+      ,.sram_be_n(sram_be_n)
+      ,.sram_oe_n(sram_oe_n)
+      ,.sram_we_n(sram_we_n)
+      );
+   defparam sram_ctrl_inst.need_wait = 1;
+`endif
+
+   rs232out rs232out_inst
+      (.clock(clk),
+       .serial_out(ttyb_txd),
+       .transmit_data(rs232out_d),
+       .we(rs232out_w),
+       .busy(rs232out_busy));
+
+   defparam rs232out_inst.frequency = FREQ,
+            rs232out_inst.bps       = BPS;
+
+
+   rs232in rs232in_inst
+      (.clock(clk),
+       .serial_in(ttyb_rxd),
+       .received_data(rs232in_data),
+       .attention(rs232in_attention));
+
+   defparam rs232in_inst.frequency = FREQ,
+            rs232in_inst.bps       = BPS;
+
+
+   rs232 rs232_inst(.clk(clk),
+               .rst(rst),
+
+               .rs232_req(rs232_req),
+               .rs232_res(rs232_res),
+
+               .rs232in_attention(rs232in_attention),
+               .rs232in_data(rs232in_data),
+
+               .rs232out_busy(rs232out_busy),
+               .rs232out_w(rs232out_w),
+               .rs232out_d(rs232out_d));
+
+`ifdef SIMULATE_MAIN
+   always #50 clk = ~clk;
+   initial #50 clk = 0;
+`endif
+endmodule
---
+++ Icarus/tinymon.c
@@ -0,0 +1,443 @@
+/*
+  This is a tiny program loader that takes it's input through the
+  serial port.
+*/
+
+typedef void (*func_t)(void);
+
+#if !defined(HOSTTEST)
+
+asm("        .globl _start       ");
+asm("_init:                      ");
+asm("_start: la    $28,_gp       ");
+asm("        la    $29,_gp+8192  ");/* + 8KiB */
+asm("        jal   main          ");
+asm("        nop                 ");
+asm("        break               ");
+
+extern void set_leds(unsigned v);
+asm(".globl set_leds;set_leds: mtlo $4;jr $31");
+
+#define RS232IN_DATA (*(volatile unsigned *) 0xFF000004)
+#define RS232IN_TAG  (*(volatile unsigned *) 0xFF000008)
+#define TSC          (*(volatile unsigned *) 0xFF00000C)
+#define SER_OUTBUSY() (*(volatile unsigned *)0xFF000000 != 0)
+#define SER_OUT(data) (*(volatile unsigned *)0xFF000000 = (data))
+
+#define BUF_SIZE 1024 // must be a power of two
+
+unsigned last_serial_tag;
+char serial_buffer[BUF_SIZE];
+unsigned serial_buffer_wp;
+unsigned serial_buffer_rp;
+
+void init_serial(void)
+{
+        last_serial_tag = RS232IN_TAG;
+        serial_buffer_rp = serial_buffer_wp = 1;
+}
+
+void check_serial_input(void)
+{
+        unsigned char tag = RS232IN_TAG;
+        if (tag != last_serial_tag) {
+                serial_buffer[serial_buffer_wp++] = RS232IN_DATA;
+                serial_buffer_wp &= BUF_SIZE-1;
+                last_serial_tag = tag;
+                if (serial_buffer_wp == serial_buffer_rp) {
+                        while (SER_OUTBUSY());
+                        SER_OUT('<');
+                        while (SER_OUTBUSY());
+                        SER_OUT('#');
+                        while (SER_OUTBUSY());
+                        SER_OUT('>');
+                }
+        }
+}
+
+void serial_out(unsigned ch)
+{
+        check_serial_input();
+
+        if (ch == '\n')
+                serial_out('\r');
+
+        check_serial_input();
+        while (SER_OUTBUSY())
+                check_serial_input();
+        SER_OUT(ch);
+        set_leds(ch);
+}
+
+unsigned char serial_in(void)
+{
+        unsigned char tag, ch;
+
+        do
+                check_serial_input();
+        while (serial_buffer_wp == serial_buffer_rp);
+
+        ch = serial_buffer[serial_buffer_rp++];
+        serial_buffer_rp &= BUF_SIZE-1;
+
+        set_leds(ch);
+
+        return ch;
+}
+
+#define store4(addr, v) (*(volatile unsigned *)(addr) = (v))
+#define load4(addr)     (*(volatile unsigned *)(addr))
+
+#else
+
+void init_serial(void) { }
+void serial_out(unsigned ch) { putchar(ch); }
+unsigned char serial_in(void) {
+        int c = getchar();
+        if (c < 0)
+                exit(0);
+}
+
+unsigned memory[1024*1024/4];
+#define store4(addr, v) (memory[(unsigned) (addr) / 4 & (1 << 18) - 1] = (v))
+#define load4(addr)     (memory[(unsigned) (addr) / 4 & (1 << 18) - 1])
+
+#endif
+
+unsigned char serial_in_lowercase(void)
+{
+        unsigned char ch = serial_in();
+
+        if (ch == '\r')
+                ch = '\n';
+
+        if ('A' <= ch && ch <= 'Z')
+                ch = ch - 'A' + 'a';
+
+        return ch;
+}
+
+unsigned char c, chk;
+unsigned in_error;
+
+void print_hex2(unsigned char);
+
+static void out4(unsigned ch4)
+{
+        if (ch4 >> 8)
+                out4(ch4 >> 8);
+        serial_out(ch4 & 0xFF);
+}
+
+static inline void print_hex1(unsigned d)
+{
+        serial_out(d + (d < 10 ? '0' : 'A' - 10));
+}
+
+void print_hex2(unsigned char v)
+{
+        print_hex1((v >> 4) & 15);
+        print_hex1(v & 15);
+}
+
+void print_hex8(unsigned v)
+{
+        int i;
+
+        for (i = 8; i; --i) {
+                print_hex1((v >> 28) & 0xF);
+                v <<= 4;
+        }
+}
+
+void print_dec(unsigned v)
+{
+        if (v >= 10)
+                print_dec(v / 10);
+        serial_out((v % 10) + '0');
+}
+
+unsigned get_hexnum(void)
+{
+        unsigned arg;
+
+        for (arg = 0;;) {
+                if ('0' <= c && c <= '9')
+                        arg = (arg << 4) + c - '0';
+                else if ('a' <= c && c <= 'f')
+                        arg = (arg << 4) + c - ('a' - 10);
+                else
+                        break;
+                chk += c;
+                c = serial_in_lowercase();
+        }
+
+        return arg;
+}
+
+unsigned d85(unsigned v)
+{
+    if (in_error)
+        goto fail;
+
+    unsigned char c;
+    do
+            c = serial_in();
+    while (c <= ' ');
+
+    if (c < '(' || '|' < c)
+        goto fail;
+
+    unsigned nv = v*85 + c - '(';
+
+    // Check for overflow
+    if (nv < v)
+        goto fail;
+
+    return nv;
+
+fail:
+    in_error = 1;
+    return 0;
+}
+
+unsigned get_base85_word(void)
+{
+    return d85(d85(d85(d85(d85(0)))));
+}
+
+#if defined(HOSTTEST)
+void tinymon_encode_word_base85(unsigned w)
+{
+        unsigned e = w % 85; w /= 85;
+        unsigned d = w % 85; w /= 85;
+        unsigned c = w % 85; w /= 85;
+        unsigned b = w % 85; w /= 85;
+        unsigned a = w % 85; w /= 85;
+        putchar(a + '(');
+        putchar(b + '(');
+        putchar(c + '(');
+        putchar(d + '(');
+        putchar(e + '(');
+}
+#endif
+
+int main()
+{
+ restart: ;
+
+        unsigned *addr = 0;
+        in_error = 0;
+
+        /* This is run out of the I$, which in practice means a ROM
+         * that cannot even be read. We must manually initialize all
+         * data and we can't use strings!
+         */
+
+        init_serial();
+
+        serial_out('\n');
+        serial_out('\n');
+
+        /*
+         * Very simple protocol
+         *
+         *   <cmd> <hex8> ' ' <hex2> '\n'
+         *
+         * The <hex2> byte is the checksum of <cmd> + each byte of
+         * <hex8>. If a mismatch is detected all input is ignored
+         * until a clear command is seen.
+         *
+         * Commands
+         * C - clear the error state (arg ignored)
+         * L - set the load address
+         * W - write a word to the current load address and move it forwards
+         * R - read a word from the current load address and move it forwards
+         * E - execute starting at the given address
+         * X - receive a block of binary data in base85 encoding
+         */
+
+        for (;;) {
+                unsigned arg = 0;
+                unsigned i;
+                unsigned char cmd, chk_ext;
+                unsigned char error_code = ' ';
+
+                do {
+                        out4('Tiny');
+                        out4('mon ');
+                        out4('2010');
+                        out4('-09-');
+                        out4('03\n');
+
+                        do
+                            c = serial_in_lowercase();
+                        while (c == '\r' || c == ' ');
+                } while (c == '\n');
+
+                /* Skip cruft until a command is encountered. */
+                while (c != 'c' && c != 'l' && c != 'w' && c != 'r' && c != 'e' &&
+                       c != 't' && c != 'x')
+                        c = serial_in_lowercase();
+
+                chk = cmd = c;
+
+                do
+                        c = serial_in_lowercase();
+                while (c == ' ');
+
+                arg = get_hexnum();
+
+                while (c == ' ')
+                        c = serial_in_lowercase();
+
+                if (c != '\n') {
+                        unsigned char good_chk = chk;
+                        // Non-interactive use
+
+                        chk_ext = get_hexnum();
+
+                        if (good_chk != chk_ext) {
+                                serial_out('<');
+                                print_hex2(good_chk);
+                                serial_out('!');
+                                serial_out('=');
+                                print_hex2(chk_ext);
+                                serial_out('>');
+
+                                error_code = '1';
+                                goto error;
+                        }
+
+                        while (c == ' ')
+                                c = serial_in_lowercase();
+
+                        if (c != '\n') {
+                                error_code = '2';
+                                goto error;
+                        }
+                }
+
+                if (in_error && cmd != 'c' && cmd != 't')
+                        continue;
+
+                if (cmd == 'c')
+                        in_error = 0;
+                else if (cmd == 'l')
+                        addr = (unsigned *) arg;
+#if 0
+                else if (cmd == 'w') {
+                        store4(addr, arg);
+                        if (load4(addr) != arg) {
+                            serial_out('!');
+                            goto error;
+                        }
+                        addr++;
+                }
+#endif
+                else if (cmd == 'r') {
+                        print_hex8(load4(addr++));
+                        serial_out('\n');
+                }
+                else if (cmd == 'e') {
+                        out4('\nE!\n');
+#if defined(HOSTTEST)
+                        printf("Execute from %08x\n", arg);
+#else
+                        ((func_t)arg)();
+#endif
+                        goto restart;
+                } else if (cmd == 'x') {
+                        unsigned *end = addr + arg;
+                        unsigned chk = 0;
+                        unsigned k = 0;
+
+                        serial_out('\n');
+                        for (k = 1; addr != end; addr += 1, ++k) {
+                                unsigned w = get_base85_word();
+                                if (in_error) {
+                                        error_code = '8';
+                                        goto error;
+                                }
+                                chk += w;
+                                store4(addr, w);
+
+                                if (((unsigned) addr & (1 << 12) - 1) == 0)
+                                    serial_out('\r'), print_dec(100 * k / arg), serial_out('%');
+                        }
+                        serial_out('\r');
+                        print_dec(100);
+                        serial_out('%');
+                        serial_out('\n');
+
+                        k = get_base85_word();
+                        if (k != -chk) {
+#if defined(HOSTTEST)
+                                printf("Got:");
+                                tinymon_encode_word_base85(k);
+                                printf("\nWanted:");
+                                tinymon_encode_word_base85(-chk);
+                                printf("\nRest: ");
+                                while (~0 != (k = getchar()))
+                                       putchar(k);
+#endif
+                                error_code = '7';
+                                goto error;
+                        }
+#if 0
+                } else if (cmd == 't') {
+                    out4('Mem ');
+                    out4('test');
+                    serial_out('\n');
+
+                    /* Simple memory tester */
+                    for (addr  = (unsigned *) 0x40000000;
+                         addr != (unsigned *) 0x400E0000;
+                         addr += 4) {
+
+                            store4(addr, ((unsigned) addr >> 13) ^~ (unsigned) addr);
+                            store4(addr + 1, ~ (unsigned) addr);
+                            store4(addr + 2, 42 + (unsigned) addr);
+                            store4(addr + 3, - (unsigned) addr);
+                    }
+
+                    for (addr  = (unsigned *) 0x40000000;
+                         addr != (unsigned *) 0x400E0000;
+                         addr += 4) {
+
+                        unsigned a, b, c, d;
+                        a = ((unsigned) addr >> 13) ^~ (unsigned) addr;
+                        b = ~ (unsigned) addr;
+                        c = 42 + (unsigned) addr;
+                        d = - (unsigned) addr;
+
+                        if (a != load4(addr) ||
+                            b != load4(addr + 1) ||
+                            c != load4(addr + 2) ||
+                            d != load4(addr + 3)) {
+                                out4('Memo');
+                                out4('ry e');
+                                out4('rror');
+                                out4(' at ');
+                                print_hex8((unsigned) addr);
+                                serial_out('\n');
+                                goto restart;
+                        }
+                    }
+                    out4('Ok!\n');
+                    continue;
+#endif
+                }
+
+                serial_out('.');
+                continue;
+
+        error:
+                serial_out('<');
+                serial_out(error_code);
+                serial_out('>');
+                in_error = 1;
+                while (c != '\n')
+                        c = serial_in_lowercase();
+        }
+}
